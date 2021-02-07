a58d30159ac71a5283b3b643c8455972d85dc7ec
DawsonWhipple <dlwhip@outlook.com>
2021-02-07 11:55:20 -0800

    First test of commiting to git on Ubunut linux

---
+++ HelloWorld.c
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "This is my first test of clion in ubuntu linux" << std::endl;
+    return 0;
+}
